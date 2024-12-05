ef1839c54ff8b2e61c0020adfa51a43e6d4a6c3c
dev-jaewon <app235naver@gmail.com>
2024-12-05 15:51:33 +0900

    refact layout design

--- frontend/src/ui/common/Layout.tsx
+++ frontend/src/ui/common/Layout.tsx
@@ -1,7 +1,14 @@
-export const Layout = ({ children }: { children: React.ReactNode }) => {
+type LayoutProps = {
+  className?: string;
+  children: React.ReactNode;
+};
+
+export const Layout = ({ children, className }: LayoutProps) => {
   return (
-    <div className="flex justify-center min-h-screen">
-      <div className="max-w-[600px] flex-1">{children}</div>
+    <div className={`flex justify-center min-h-screen ${className || ""}`}>
+      <div className="flex flex-col max-w-[600px] flex-1 bg-[#f5f5f5]">
+        {children}
+      </div>
     </div>
   );
 };
